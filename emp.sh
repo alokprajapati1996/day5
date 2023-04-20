2921a0601487480fbeb1c760e0be9a9c3f70ac43
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-04-20 21:38:31 +0530

    Alok: Added day_5 Assignment

---
+++ emp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+isPersent=1;
+
+if [ $isPersent -eq 1 ]
+then
+echo "employee was persent"
+else
+ echo "employee was not persent"
+fi
---
+++ random.sh
@@ -0,0 +1,4 @@
+ #!/bin/bash -x
+random1=$((RANDOM%100))
+ 
+echo "value of random1=$random1"
