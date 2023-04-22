b195a6543749e7d2a055e8db7caefb06006decc3
Alok Kumar Prajapati <122681546+alokprajapati1996@users.noreply.github.com>
2023-04-22 19:26:35 +0530

    Add files via upload

---
+++ sum_avg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+random1=$((RANDOM%90+10))
+random2=$((RANDOM%90+10))
+random3=$((RANDOM%90+10))
+random4=$((RANDOM%90+10))
+random5=$((RANDOM%90+10))
+sum=$((random1+random2+random3+random4+random5))
+
+
+avg=$((sum/5))
+
+
+
